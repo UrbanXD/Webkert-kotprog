4d6cd7cfc75de7567eade803f0e42c5b8087862c
Urbán Ádám <h270385@stud.u-szeged.hu>
2024-02-22 20:47:27 +0100

    nav design + some changes on main page

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainComponent} from "./main.component";
 import {RouterModule, Routes} from "@angular/router";
+import {FlexModule} from "@angular/flex-layout";
 
 const routes: Routes = [{ path: '', component: MainComponent }]
 
@@ -9,9 +10,10 @@ const routes: Routes = [{ path: '', component: MainComponent }]
   declarations: [
     MainComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        FlexModule
+    ]
 })
 export class MainModule { }
