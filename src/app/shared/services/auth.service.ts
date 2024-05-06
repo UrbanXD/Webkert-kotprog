0656019f4f585112ac10801f79741e80a68f2ef5
Urbán Ádám <h270385@stud.u-szeged.hu>
2024-05-06 20:41:16 +0200

    changes

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: AngularFireAuth) { }
+
+  isUserLoggedIn(){
+    return this.auth.user;
+  }
+
+  register(email: string, password: string){
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+  login(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout(){
+    this.auth.signOut();
+  }
+}
